f2ef827119576c5f0101597a483864229052df5f
Laurent Coustet <ed@zehome.com>
2012-04-12 22:25:53 +0200

    Test smaller header

--- src/pkt.h
+++ src/pkt.h
@@ -5,15 +5,12 @@
 
 /* TCP overhead = 66 Bytes on the wire */
 #define DEFAULT_MTU 1500
-#define TCP_OVERHEAD 66
-#define TUNTAP_RW_MAX DEFAULT_MTU
-#define RTUN_RW_MAX (DEFAULT_MTU - TCP_OVERHEAD)
 #define MAX_PKT_LEN 1500
 
 struct mlvpn_pktdata
 {
-    uint32_t magic;
-    uint32_t len;
+    uint8_t magic;
+    uint16_t len;
     char data[DEFAULT_MTU];
 };
 #define PKTHDRSIZ(pktdata) (sizeof(pktdata)-sizeof(pktdata.data))
