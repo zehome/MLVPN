e4840b3698db44dc22e52f2546dce0f739c31d33
Laurent Coustet <ed@zehome.com>
2012-05-28 23:38:05 +0200

    When we receive data, we could receive "many" packets at once. Now the parser try to "decapsulate" as many packets as available

--- src/mlvpn.c
+++ src/mlvpn.c
@@ -995,7 +995,8 @@ int mlvpn_rtun_read(mlvpn_tunnel_t *tun)
             }
         }
         tun->rbuf.len += len;
-        mlvpn_rtun_tick_rbuf(tun);
+        /* THX philpep for this optimisation. Snake eyes ;-) */
+        while (mlvpn_rtun_tick_rbuf(tun) > 0);
     } else if (len < 0) {
         _ERROR("Read error on %d: %s\n", tun->fd, strerror(errno));
         mlvpn_rtun_status_down(tun);
