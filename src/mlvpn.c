9363f52ffd654c5580a03869dd2772e26ac7b79e
Laurent Coustet <ed@zehome.com>
2012-06-22 21:32:39 +0200

    Hum, mistake commit

--- src/mlvpn.c
+++ src/mlvpn.c
@@ -993,6 +993,7 @@ int mlvpn_rtun_read(mlvpn_tunnel_t *tun)
                 _DEBUG("< TUN %d read %d bytes from %s:%s.\n", tun->fd, len,
                             clienthost, clientport);
             }
+            mlvpn_rtun_tick_rbuf(tun);
         }
         tun->rbuf.len += len;
     } else if (len < 0) {
