0e8486052eb2fca5e8a79445a384ff37a80b08b4
Laurent Coustet <ed@zehome.com>
2012-09-24 19:36:46 +0200

    pktdata->len can't be < 0, that's an uint

--- src/tuntap_generic.c
+++ src/tuntap_generic.c
@@ -7,6 +7,7 @@ int mlvpn_tuntap_read(struct tuntap_s *tuntap)
     pktbuffer_t *sbuf;
     mlvpn_tunnel_t *rtun;
     mlvpn_pkt_t *pkt;
+    int ret;
 
     /* choosing a tunnel to send to (direct buffer copy) */
     rtun = mlvpn_rtun_choose();
@@ -25,19 +26,20 @@ int mlvpn_tuntap_read(struct tuntap_s *tuntap)
 
     /* Ask for a free buffer (protected by the check just above) */
     pkt = mlvpn_get_free_pkt(sbuf);
-    pkt->pktdata.len = read(tuntap->fd, pkt->pktdata.data, DEFAULT_MTU);
+    ret = read(tuntap->fd, pkt->pktdata.data, DEFAULT_MTU);
 
-    if (pkt->pktdata.len < 0)
+    if (ret < 0)
     {
         /* read error on tuntap is not recoverable. We must die. */
         _FATAL("[tuntap %s] unrecoverable read error: %s\n",
             tuntap->devname, strerror(errno));
         exit(1);
-    } else if (pkt->pktdata.len == 0) {
+    } else if (ret == 0) {
         /* End of file */
         _FATAL("[tuntap %s] unrecoverable error (reached EOF on tuntap!)\n");
         exit(1);
     }
+    pkt->pktdata.len = ret;
 
     return pkt->pktdata.len;
 }
