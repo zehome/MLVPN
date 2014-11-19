815151dc46ab744ced037fb4dc34a15f1c3940ce
Laurent Coustet <laurent.coustet@clarisys.fr>
2014-11-19 12:05:37 +0100

    uint can't be negative.

--- src/privsep.c
+++ src/privsep.c
@@ -273,7 +273,7 @@ priv_init(char *argv[], char *username)
                     _exit(0);
 
             must_read(socks[0], &len, sizeof(len));
-            if (len < 0 || len > MLVPN_IFNAMSIZ)
+            if (len > MLVPN_IFNAMSIZ)
                 _exit(0);
             else if (len > 0) {
                 must_read(socks[0], &tuntapname, len);
