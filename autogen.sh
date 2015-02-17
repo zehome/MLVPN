a33d38b33cd09b2ab7d6eff1b831fb955ddd1b87
Laurent Coustet <ed@zehome.com>
2015-02-17 01:43:02 +0100

    "fix" pkg-config for systemd. Must try on other platforms.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,20 @@
 #!/bin/sh -e
 
+AUTORECONF=${AUTORECONF:-autoreconf}
+ACLOCAL=${ACLOCAL:-aclocal}
+AUTOCONF=${AUTOCONF:-autoconf}
+AUTOMAKE=${AUTOMAKE:-automake}
+
+# Check we have all tools installed
+check_command() {
+    command -v "${1}" > /dev/null 2>&1 || {
+        >&2 echo "autogen.sh: could not find \`$1'. \`$1' is required to run autogen.sh."
+        exit 1
+    }
+}
+check_command "$AUTORECONF"
+check_command "$ACLOCAL"
+check_command "$AUTOCONF"
+check_command "$AUTOMAKE"
+
 exec autoreconf -fi
