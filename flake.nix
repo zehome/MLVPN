63690ab292d238840d2bca7b17d76ec6b215e132
DavHau <hsngrmpf+github@gmail.com>
2021-03-25 14:08:14 +0700

    refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   };
 
   outputs = inp:
-    let 
+    let
+      lib = inp.nixpkgs.lib;
       systems = [ "aarch64-linux" "armv7l-linux" "x86_64-linux" ];
+      
       pkgsForSystem = system: inp.nixpkgs.legacyPackages."${system}";
       pkgsCross = system: crossSystem: import inp.nixpkgs {
         inherit system crossSystem;
       };
-      lib = inp.nixpkgs.lib;
       buildFor = pkgs: pkgs.stdenv.mkDerivation {
         src = ./.;
         name = "mlvpn";
@@ -31,25 +32,28 @@
     in
       rec {
         defaultPackage = lib.genAttrs systems (system: packages."${system}".mlvpn);
-        packages = lib.genAttrs systems (system: 
+        packages = lib.genAttrs systems (system:
+          let 
+            otherSystems = builtins.filter (s: s != system) systems;
+          in
           {
             mlvpn = buildFor (pkgsForSystem system);
             mlvpn-static = buildFor (pkgsForSystem system).pkgsStatic;
           }
 
           # cross compiled packages
-          // (lib.listToAttrs (map (crossSystem:
-            lib.nameValuePair
-              "mlvpn-${crossSystem}"
-              (buildFor (pkgsCross system crossSystem))
-          ) (builtins.filter (s: s != system) systems) ))
+          //
+          (lib.listToAttrs (map (crossSystem: lib.nameValuePair
+            "mlvpn-${crossSystem}"
+            (buildFor (pkgsCross system crossSystem))
+          ) otherSystems ))
           
           # cross compiled static packages
-          // (lib.listToAttrs (map (crossSystem:
-            lib.nameValuePair
-              "mlvpn-static-${crossSystem}"
-              (buildFor (pkgsCross system crossSystem).pkgsStatic)
-          ) (builtins.filter (s: s != system) systems) ))
+          //
+          (lib.listToAttrs (map (crossSystem: lib.nameValuePair
+            "mlvpn-static-${crossSystem}"
+            (buildFor (pkgsCross system crossSystem).pkgsStatic)
+          ) otherSystems ))
         );
       };
 }
